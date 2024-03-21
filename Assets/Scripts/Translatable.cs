0b85ee3b861fed3873a1bbf1ca34bbf947906c6b
Antonio José Velez Agosto <iamphyxics@gmail.com>
2024-03-21 12:10:04 -0400

    further data hiding

--- Assets/Scripts/Translatable.cs
+++ Assets/Scripts/Translatable.cs
@@ -3,4 +3,6 @@ public interface Translatable
 	public string RuStr ();
 	public string EnStr ();
 	public Translatable Next ();
+//	public bool Belongs (string word);
+//	public string GetType (string word);
 }
